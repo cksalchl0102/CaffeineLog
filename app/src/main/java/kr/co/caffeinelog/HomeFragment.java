cebf325f4079f782add75b674009745dca3295bd
CHOIchanmi <zaqs0828@hansung.ac.kr>
2019-11-29 03:02:17 +0900

    전체적인 틀 입니다. 각각의 프래그먼트 비교를 위해 배경색을 다르게 설정했어요. 각자 프래그먼트에서 코딩하면 됩니당.

---
+++ app/src/main/java/kr/co/caffeinelog/HomeFragment.java
@@ -0,0 +1,18 @@
+package kr.co.caffeinelog;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+}
